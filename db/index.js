109f81dbbc5e79dae8eb0f1b446d6219a91a0135
lotsuki <otsuki.lindsey@gmail.com>
2018-11-29 16:25:58 -0800

    edit db connection file

--- db/index.js
+++ db/index.js
@@ -1,11 +1,11 @@
-const { Pool, Client } = require('pg');
-var connectionStr = 'postgres://lindseyotsuki@127.0.0.1:5432/homeshare'
-const db = new Client(connectionStr);
+// const { Pool, Client } = require('pg');
+// var connectionStr = 'postgres://postgres@ec2-3-16-108-112.us-east-2.compute.amazonaws.com:5432/airjld'
+// const db = new Client(connectionStr);
 
-db.connect();
+// db.connect();
 
 
-module.exports = db;
+// module.exports = db;
 
 
 
