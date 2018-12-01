f5f5a4f839af6f78d034477a76c3b7388963228f
lotsuki <otsuki.lindsey@gmail.com>
2018-11-30 20:00:44 -0800

    update connectionstr

--- db/index.js
+++ db/index.js
@@ -1,5 +1,5 @@
 const { Pool, Client } = require('pg');
-var connectionStr = 'postgres://postgres:password@ec2-3-16-108-112.us-east-2.compute.amazonaws.com/airjld'
+var connectionStr = 'postgres://postgres:password@ec2-18-216-31-87.us-east-2.compute.amazonaws.com:5432/airjld'
 const db = new Client(connectionStr);
 
 db.connect();
