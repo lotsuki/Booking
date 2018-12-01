b7e10d6ac653ab900b8bb8962e7fea9416fcb937
lotsuki <otsuki.lindsey@gmail.com>
2018-11-30 21:07:59 -0800

    update;

--- db/index.js
+++ db/index.js
@@ -1,5 +1,7 @@
 const { Pool, Client } = require('pg');
-var connectionStr = 'postgres://postgres:password@ec2-18-216-31-87.us-east-2.compute.amazonaws.com:5432/airjld'
+var connectionStr = 'postgres://postgres@ec2-18-216-31-87.us-east-2.compute.amazonaws.com:5432/airjld'
+
+// var connectionStr = 'postgres://lindseyotsuki@127.0.0.1:5432/homeshare'
 const db = new Client(connectionStr);
 
 db.connect();
